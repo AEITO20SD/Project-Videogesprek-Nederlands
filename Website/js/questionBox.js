03ca5e253c5b38f139521466f9a0d50ed038971c
Lucas <71124462+LucasHorizon@users.noreply.github.com>
2022-10-01 18:01:41 +0200

    Questionbox

---
+++ Website/js/questionBox.js
@@ -0,0 +1,9 @@
+function openQuestion() {
+    var e = document.getElementById("questionBox");
+    e.style.display = "block";
+} 
+
+function closeQuestion() {
+    var e = document.getElementById("questionBox");
+    e.style.display = "none";
+} 
