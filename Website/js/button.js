9c90bd862e067e8245831d5385bc347cf86d7ced
Lucas <71124462+LucasHorizon@users.noreply.github.com>
2022-10-28 17:21:45 +0200

    Validation boxes

--- Website/js/button.js
+++ Website/js/button.js
@@ -9,12 +9,16 @@ function closeQuestion() {
 } 
 
 function checkInput() {
-    var video = document.getElementsByClassName("dropzone");
-    // const videoBox = document.querySelector('.js-dropzone');
+    // var videos = document.querySelectorAll('.js-dropzone');
+    const videoBox = document.querySelector('.js-dropzone');
+    const videoBoxTwo = document.querySelector('#dropzone-1');
+    const videoBoxThree = document.querySelector('#dropzone-2');
+    const videoBoxFour = document.querySelector('#dropzone-3');
+    const videoBoxFive = document.querySelector('#dropzone-4');
     var success = true;
 
     // for(var video in videos){
-    //     if (video.childNodes.length <= 0) {
+    //     if (videos.childNodes.length <= 0) {
     //         success = false;
     //         setErrorFor(video);
     //     } else {
@@ -23,27 +27,53 @@ function checkInput() {
     //     return success;
     // }   
 
-    for(var x = 0; x < video.length; x++){
-        if (video.childNodes.length <= 0) {
-            success = false;
-            setErrorFor(video);
-        } else {
-            setSuccessFor(video);
-        }
-        return success;
-    }   
+    // for(var i = 0; i < child.length; i++){
+    //     if (child.length <= 0) {
+    //         success = false;
+    //         setErrorFor(videos);
+    //     } else {
+    //         setSuccessFor(videos);
+    //     }
+    //     return success;
+    // }   
 
-    // if (videoBox.childNodes.length <= 0) {
-    //     success = false;
-    //     setErrorFor(videoBox)
-    // } else {
-    //     setSuccessFor(videoBox)
-    // }
+    if (videoBox.childNodes.length <= 0) {
+        success = false;
+        setErrorFor(videoBox)
+    } else {
+        setSuccessFor(videoBox)
+    }
+
+    if (videoBoxTwo.childNodes.length <= 0) {
+        success = false;
+        setErrorFor(videoBoxTwo)
+    } else {
+        setSuccessFor(videoBoxTwo)
+    }
+
+    if (videoBoxThree.childNodes.length <= 0) {
+        success = false;
+        setErrorFor(videoBoxThree)
+    } else {
+        setSuccessFor(videoBoxThree)
+    }
+
+    if (videoBoxFour.childNodes.length <= 0) {
+        success = false;
+        setErrorFor(videoBoxFour)
+    } else {
+        setSuccessFor(videoBoxFour)
+    }
+
+    if (videoBoxFive.childNodes.length <= 0) {
+        success = false;
+        setErrorFor(videoBoxFive)
+    } else {
+        setSuccessFor(videoBoxFive)
+    }
 }
 
-function setErrorFor() {
-    // const boxControl = document.querySelector('.js-dropzone');
-    var boxControl = document.getElementsByClassName("dropzone");
+function setErrorFor(boxControl) {
     boxControl.style.borderColor = "#ff2d4a";
     const small = boxControl.parentElement.querySelector('small');
     small.style.display = "block";
@@ -52,9 +82,7 @@ function setErrorFor() {
     span.style.display = "none";
 }
 
-function setSuccessFor() {
-    // const boxControl = document.querySelector('.js-dropzone');
-    var boxControl = document.getElementsByClassName("dropzone");
+function setSuccessFor(boxControl) {
     boxControl.style.borderColor = "white";
     const small = boxControl.parentElement.querySelector('small');
     small.style.display = "none";
