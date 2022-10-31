2e431ec14702acf38896fa5ed8087a4f5241b1a6
JustinAlink <161537@student.horizoncollege.nl>
2022-10-31 08:34:42 +0100

    test appending from json randomly

--- Website/js/getVideos.js
+++ Website/js/getVideos.js
@@ -1,10 +1,5 @@
 $.get('./data/videos.JSON', function(d) {
-  
   shuffledVideoList = shuffle(d.videos);
-
-  console.log(d);
-  console.log(shuffledVideoList);
-
 
   //Source: https://stackoverflow.com/a/3718452 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
   function shuffle(sourceArray) {
@@ -18,20 +13,63 @@ $.get('./data/videos.JSON', function(d) {
     return sourceArray;
 }
 
-
-  for(let i = 0; i < Object.keys(d.videos).length; i++) {
+  for(let i = 0; i < Object.keys(shuffledVideoList).length; i++) {
     if(i % 5 === 0) {
-      console.log("yes");
+      console.log("should make new row");
+      k = 0;
+      k++;
+      console.log(shuffledVideoList[i]);
+
+      $(".js-videos").append(`<div class="row videos js-row">`);
+      $(".js-row[k]").append(videoCodeBlock);
 
     }
     else {
-      console.log("no");
+      console.log("doesn't make a new row");
+      console.log(shuffledVideoList[i]);
+
+      $(".js-row").append(videoCodeBlock);
 
     }
   }
 
 });
 
+
+videoCodeBlock = `
+  <div class="col">
+  <div class="video js-video drag-drop">
+    Videos
+    <span class="remove js-remove">
+      <svg class="delete"
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            viewBox="0 0 34.56 34.56"
+      ><path
+                id="Icon_map-circle"
+                data-name="Icon map-circle"
+                d="M35.28,18A17.28,17.28,0,1,1,18,.72,17.28,17.28,0,0,1,35.28,18Z"
+                transform="translate(-0.72 -0.72)"
+        /><svg
+            class="delete-cross"
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            viewBox="0 0 48 48"
+    ><path
+                id="Icon_metro-cross"
+                data-name="Icon metro-cross"
+                d="M33.138,26.711h0l-9.358-9.358,9.358-9.358h0a.966.966,0,0,0,0-1.363L28.717,2.21a.967.967,0,0,0-1.363,0h0L18,11.568,8.637,2.21h0a.966.966,0,0,0-1.363,0L2.852,6.631a.966.966,0,0,0,0,1.363h0l9.358,9.358L2.852,26.711h0a.966.966,0,0,0,0,1.363l4.421,4.421a.966.966,0,0,0,1.363,0h0L18,23.136l9.358,9.358h0a.966.966,0,0,0,1.363,0l4.421-4.421a.966.966,0,0,0,0-1.363Z"
+                transform="translate(15 15)"
+                fill="#fff"
+        />
+      </svg>
+      </svg>
+    </span>
+  </div>
+  </div>
+`; 
 
 
 //   d.videos.forEach(function(b) {
