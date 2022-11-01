f92ffd933e46c90c3226776c10e4971ba045dda9
JustinAlink <161537@student.horizoncollege.nl>
2022-11-01 09:44:59 +0100

    fetching opdrachten

--- Website/js/getVideos.js
+++ Website/js/getVideos.js
@@ -1,90 +1,128 @@
-document.addEventListener("DOMContentLoaded", async function() {
-  const request = await (await fetch("./data/videos.JSON"));
-  const json = await request.json();
+let assignment;
 
-  placeVideos(json);
+document.addEventListener("DOMContentLoaded", async function () {
+  const url = new URL(window.location.href);
+  const assignmentId = url.searchParams.get("id") || 1;
+
+  const assignmentRequest = await fetch("./data/opdrachten.JSON");
+  const assignmentJson = await assignmentRequest.json();
+
+  assignment = assignmentJson.opdrachten.find((e) => e.id == assignmentId);
+
+  if (!assignment) {
+    return;
+  }
+
+  console.log(assignment);
+
+  const mediaLibraryRequest = await fetch("./data/videos.JSON");
+  const mediaLibraryJson = await mediaLibraryRequest.json();
+
+  const videos = mediaLibraryJson.videos.filter(function(e) {
+    //Bepaal of deze video in de lijst van te tonen videos zit
+    return assignment.listOfVideos.some(function(videoId) {
+      return videoId == e.id; 
+    });
+  });
+
+  placeVideos(videos);
 
   //document.dispatchEvent(new CustomEvent("Custom-VideosLoaded"));
   afterVideosLoaded();
 });
-let i = 0;
-let k = 0;
 
+function placeVideos(videos) {
+  let shuffledVideoList = shuffle(videos);
 
-function placeVideos(d) {
-  shuffledVideoList = shuffle(d.videos);
+  let k = 0;
 
-  //Source: https://stackoverflow.com/a/3718452 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
-  function shuffle(sourceArray) {
-    for (var i = 0; i < sourceArray.length - 1; i++) {
-        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+  for (let i = 0; i < shuffledVideoList.length; i++) {
+    let currentVideo = shuffledVideoList[i];
 
-        var temp = sourceArray[j];
-        sourceArray[j] = sourceArray[i];
-        sourceArray[i] = temp;
-    }
-    return sourceArray;
-}
+    const videoBlock = document
+      .getElementById("video-block")
+      .content.cloneNode(true);
+    videoBlock.querySelector(".js-video").dataset["videoId"] = currentVideo.id;
+    videoBlock.querySelector(".js-video-source").src = currentVideo.url;
+    videoBlock.querySelector(".js-title").innerHTML = `Videos
+    ${currentVideo.id}
+    ${currentVideo.name}
+    ${currentVideo.url}
+    ${currentVideo.category}
+    ${currentVideo.tone}`;
 
-  for(let i = 0; i < Object.keys(shuffledVideoList).length; i++) {
-    currentVideo = shuffledVideoList[i];
-    videoCodeBlock = `
-      <div class="col">
-      <div class="video js-video drag-drop" onclick="openVideo()">
-        Videos
-        ${currentVideo.id}
-        ${currentVideo.name}
-        ${currentVideo.url}
-        ${currentVideo.category}
-        ${currentVideo.tone}
-        <span class="remove js-remove">
-          <svg class="delete"
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 34.56 34.56"
-          ><path
-                    id="Icon_map-circle"
-                    data-name="Icon map-circle"
-                    d="M35.28,18A17.28,17.28,0,1,1,18,.72,17.28,17.28,0,0,1,35.28,18Z"
-                    transform="translate(-0.72 -0.72)"
-            /><svg
-                class="delete-cross"
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 48 48"
-        ><path
-                    id="Icon_metro-cross"
-                    data-name="Icon metro-cross"
-                    d="M33.138,26.711h0l-9.358-9.358,9.358-9.358h0a.966.966,0,0,0,0-1.363L28.717,2.21a.967.967,0,0,0-1.363,0h0L18,11.568,8.637,2.21h0a.966.966,0,0,0-1.363,0L2.852,6.631a.966.966,0,0,0,0,1.363h0l9.358,9.358L2.852,26.711h0a.966.966,0,0,0,0,1.363l4.421,4.421a.966.966,0,0,0,1.363,0h0L18,23.136l9.358,9.358h0a.966.966,0,0,0,1.363,0l4.421-4.421a.966.966,0,0,0,0-1.363Z"
-                    transform="translate(15 15)"
-                    fill="#fff"
-            />
-          </svg>
-          </svg>
-        </span>
-      </div>
-      </div>
-    `;
+    // let videoCodeBlock = `
+    //   <div class="col">
+    //   <div class="video js-video drag-drop" onclick="openVideo()" data-video-id="${currentVideo.id}">
+    //     Videos
+    //     ${currentVideo.id}
+    //     ${currentVideo.name}
+    //     ${currentVideo.url}
+    //     ${currentVideo.category}
+    //     ${currentVideo.tone}
+    //     <span class="remove js-remove">
+    //       <svg class="delete"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             width="25"
+    //             height="25"
+    //             viewBox="0 0 34.56 34.56"
+    //       ><path
+    //                 id="Icon_map-circle"
+    //                 data-name="Icon map-circle"
+    //                 d="M35.28,18A17.28,17.28,0,1,1,18,.72,17.28,17.28,0,0,1,35.28,18Z"
+    //                 transform="translate(-0.72 -0.72)"
+    //         /><svg
+    //             class="delete-cross"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             width="25"
+    //             height="25"
+    //             viewBox="0 0 48 48"
+    //     ><path
+    //                 id="Icon_metro-cross"
+    //                 data-name="Icon metro-cross"
+    //                 d="M33.138,26.711h0l-9.358-9.358,9.358-9.358h0a.966.966,0,0,0,0-1.363L28.717,2.21a.967.967,0,0,0-1.363,0h0L18,11.568,8.637,2.21h0a.966.966,0,0,0-1.363,0L2.852,6.631a.966.966,0,0,0,0,1.363h0l9.358,9.358L2.852,26.711h0a.966.966,0,0,0,0,1.363l4.421,4.421a.966.966,0,0,0,1.363,0h0L18,23.136l9.358,9.358h0a.966.966,0,0,0,1.363,0l4.421-4.421a.966.966,0,0,0,0-1.363Z"
+    //                 transform="translate(15 15)"
+    //                 fill="#fff"
+    //         />
+    //       </svg>
+    //       </svg>
+    //     </span>
+    //   </div>
+    //   </div>
+    // `;
 
-    if(i % 5 === 0) {
+    if (i % 5 === 0) {
       k++;
 
-      $(".js-videos").append('<div class="row videos js-row'+k+'">');
-      $(".js-row"+[k]).append(videoCodeBlock);
+      let videos = document.querySelector(".js-videos");
 
-    }
-    else {
+      let row = document.createElement("div");
+      row.classList.add("row", "videos", "js-row" + k);
+      //row.innerHTML = videoCodeBlock;
+      row.appendChild(videoBlock);
 
-      $(".js-row"+[k]).append(videoCodeBlock);
-
+      videos.appendChild(row);
+    } else {
+      //document.querySelector(".js-row" + k).innerHTML += videoCodeBlock;
+      document.querySelector(".js-row" + k).appendChild(videoBlock);
     }
   }
 }
 
+//Source: https://stackoverflow.com/a/3718452 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+function shuffle(sourceArray) {
+  for (var i = 0; i < sourceArray.length - 1; i++) {
+    var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+    var temp = sourceArray[j];
+    sourceArray[j] = sourceArray[i];
+    sourceArray[i] = temp;
+  }
+  return sourceArray;
+}
+
 function openVideo() {
-  console.log("open "+ videoCodeBlock);
+  console.log("open " + videoCodeBlock);
 }
 
 function closeVideo() {
